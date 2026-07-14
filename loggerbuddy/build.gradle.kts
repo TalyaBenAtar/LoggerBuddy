14f48a415d9d92fd62a8887b0afb31478628a51a
TalyaBenAtar <Talya159321@gmail.com>
2026-07-15 00:16:54 +0300

    Prepare LoggerBuddy 2.0.0 release

--- loggerbuddy/build.gradle.kts
+++ loggerbuddy/build.gradle.kts
@@ -65,7 +65,7 @@ afterEvaluate {
 
                 groupId = "com.github.TalyaBenAtar"
                 artifactId = "LoggerBuddy"
-                version = "1.0.0"
+                version = "2.0.0"
             }
         }
     }
